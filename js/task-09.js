367e974742b1593fd1a66a0ba32ddacceda835fb
Vitalii Dimov <113688545+Carriedbydog@users.noreply.github.com>
2023-08-07 17:01:48 +0300

    task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,17 @@
+const refs = {
+  widget: document.querySelector(".widget"),
+  spanEl: document.querySelector(".color"),
+  colorChangeBtn: document.querySelector(".change-color"),
+};
+
+refs.colorChangeBtn.addEventListener("click", getRandomHexColor);
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
+  const newColor = (document.body.style.backgroundColor = `#${Math.floor(
+    Math.random() * 16777215
+  )
     .toString(16)
-    .padStart(6, 0)}`;
+    .padStart(6, 0)}`);
+
+  return (refs.spanEl.textContent = newColor);
 }

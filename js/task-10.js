2a376110acc3219c9253e9932cb247229e44c10f
Vitalii Dimov <113688545+Carriedbydog@users.noreply.github.com>
2023-08-07 18:17:07 +0300

    task 10

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,35 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const refs = {
+  controls: document.querySelector("#controls"),
+  boxes: document.querySelector("#boxes"),
+  btnCreate: document.querySelector("[data-create]"),
+  btnDestroy: document.querySelector("[data-destroy]"),
+  input: document.querySelector("input"),
+};
+
+refs.btnCreate.addEventListener("click", createBoxes);
+refs.btnDestroy.addEventListener("click", destroyBoxes);
+
+function createBoxes() {
+  destroyBoxes();
+  const amount = refs.input.value;
+  const arr = [];
+
+  for (let i = 1; i <= amount; i++) {
+    const newBoxes = document.createElement("div");
+    newBoxes.style.width = `${30 * i}px`;
+    newBoxes.style.height = `${30 * i}px`;
+    newBoxes.style.border = "1px solid black";
+    newBoxes.style.backgroundColor = getRandomHexColor();
+
+    arr.push(newBoxes);
+  }
+  refs.boxes.append(...arr);
+  console.log(refs.boxes);
+}
+
+function destroyBoxes() {
+  refs.boxes.innerHTML = "";
+}
